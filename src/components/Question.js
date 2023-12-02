f7724132a03c8a05beb9d9e5279010a048954c45
Boniface <Cheruiyotbony@gmail.com>
2023-12-02 17:18:01 +0300

    done

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    // Set up a timeout to decrease timeRemaining every second
+    const timerId = setTimeout(() => {
+      setTimeRemaining((prevTime) => Math.max(prevTime - 1, 0));
+    }, 1000);
+
+    // Clean up the timeout and trigger onAnswered when timeRemaining hits 0
+    if (timeRemaining === 0) {
+      setTimeRemaining(10); // Reset timeRemaining for the next question
+      onAnswered(false);    // Notify the parent component about the timeout
+    }
+
+    // Clean up the timeout when the component unmounts or when the question is answered
+    return () => clearTimeout(timerId);
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
+    // Reset the timer and notify the parent component about the answer
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
